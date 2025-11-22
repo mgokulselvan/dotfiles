16d752a52b9e282f0c39af69756517c4227c3dab
Gokul Selvan <mgokulselvan@gmail.com>
2025-11-22 16:54:48 +0000

    added basic configs for nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,23 @@
+--Using Clipboard for the " register
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.number = true
+
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+
+vim.opt.shiftwidth = 4
+
+vim.opt.autoindent = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+
+vim.opt.textwidth = 50
+
+vim.opt.syntax = "on"
+
+
 require("config.lazy")
