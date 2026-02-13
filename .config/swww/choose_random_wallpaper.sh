cc02ec8e9c7252fdcd2399efa6f2e97ced8be04d
Gokul Selvan <mgokulselvan@gmail.com>
2026-02-13 16:13:50 +0000

    rofi changes, using symlink instead of a dynamically changing .rasi file for change in wallpaper

--- .config/swww/choose_random_wallpaper.sh
+++ .config/swww/choose_random_wallpaper.sh
@@ -5,9 +5,6 @@ WALLPAPER_DIR="$HOME/Wallpapers"
 
 WALLPAPER=$(ls "$WALLPAPER_DIR" | shuf -n1)
 
-rm -f ~/.config/rofi/wallpaperPath.rasi
-touch ~/.config/rofi/wallpaperPath.rasi
-printf "*{ \n		wallpaperPath:url( \"$WALLPAPER_DIR/$WALLPAPER\" ,height);\n} " > ~/.config/rofi/wallpaperPath.rasi
-chmod 444 ~/.config/rofi/wallpaperPath.rasi
+ln -sf "$WALLPAPER_DIR/$WALLPAPER" "$HOME/.config/rofi/wallpaper"
 
 [ -n "$WALLPAPER" ] && swww img -t random "$WALLPAPER_DIR/$WALLPAPER" 
